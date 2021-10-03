2dbbaf222b9d20716cfc4e35d149d88f9e8a47eb
Kåre Mølgaard Rasmussen <34368915+dk-moelgaard75@users.noreply.github.com>
2021-10-03 18:58:04 +0200

    Updated all code to make tests run

--- Skatteberegner/Beregning.cs
+++ Skatteberegner/Beregning.cs
@@ -11,7 +11,27 @@ namespace Skatteberegner
         // Funktionen returnerer det beløb, der skal beskattes (altså ikke selve skatten).
         public double SkatVedJulegave(double julegave, double andenGave)
         {
-            return 0;
+            double ret = 0;
+            double samletPris = julegave + andenGave;
+            if (julegave <= 1200 && andenGave == 0)
+            {
+                //hvis anden gave er 0, saa maa julegaven godt være 1200
+                ret = 0;
+            }
+            else if (julegave <= 900 && samletPris <= 1200)
+            {
+                ret = 0;
+            }
+            else if(julegave <= 900 && samletPris > 1200)
+            {
+                ret = andenGave;
+            }
+            else
+            {
+                ret = samletPris;
+            }
+
+            return ret;
         }
     }
 }
